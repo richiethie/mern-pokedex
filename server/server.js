77e782dc670fefe47db8bd3d230d40ff9ba9c9e1
richiethie <richiethie@gmail.com>
2023-02-06 21:13:41 -0600

    Apollo Server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const path = require('path')
 const express = require('express')
 const connection = require('./config/connection')
-const { appendFile } = require('fs')
+const { ApolloServer } = require('@apollo/server')
+const { expressMiddleware } = require('@apollo/server/express4')
+const typeDefs = require('./schemas/typeDefs')
+const resolvers = require('./schemas/resolvers')
 
 const PORT = process.env.PORT || 3001
 const app = express()
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
@@ -18,7 +24,11 @@ app.get('/', (req, res) => {
 })
 
 connection.once('open', async () => {
+    await apolloServer.start()
+    app.use('/graphql', expressMiddleware(apolloServer))
+
     app.listen(PORT, () => {
         console.log(`Express server listening on http://localhose:${PORT}`)
+        console.log(`Apollo GraphQL playground available at http://localhost:${PORT}/graphql`)
     })
 })
